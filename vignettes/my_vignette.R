5efdc557f8c01c1edeb2601ee74c7f3adc090264
aashana94 <aashananijhawan@gmail.com>
2018-10-06 14:12:23 +0200

    vinette updated

--- vignettes/my_vignette.R
+++ vignettes/my_vignette.R
@@ -4,3 +4,36 @@ knitr::opts_chunk$set(
   comment = "#>"
 )
 
+## ------------------------------------------------------------------------
+latlon <- function(address, apiKey){
+
+  param <- gsub("[\\@\\#\\%\\&\\!\\(\\)\\_\\*\\+\\;\\^]", "", address)
+  params <- trimws(gsub("\\s+","+", param))
+
+  base <- "https://maps.googleapis.com/maps/api/geocode/json?address="
+  
+  urlAPI <- paste0(base,params,"&key=",apiKey)
+  callAPI <- GET(urlAPI)
+  content(callAPI)
+  resultJSON <- fromJSON(txt = callAPI$url)
+  X <- resultJSON$results$geometry$location$lng
+  Y <- resultJSON$results$geometry$location$lat
+
+  return(c(X,Y))
+}
+
+## ------------------------------------------------------------------------
+reverselatlon<- function(lat, lon, apiKey){
+
+  params <- paste0(lat,",",lon)
+  base <- "https://maps.googleapis.com/maps/api/geocode/json?latlng="
+  
+  urlAPI <- paste0(base,params,"&key=",apiKey)
+  callAPI <- GET(urlAPI)
+  content(callAPI)
+  resultJSON <- fromJSON(txt = callAPI$url)
+  address <- resultJSON$results$formatted_address
+  
+  return(address)
+}
+
