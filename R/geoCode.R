a9f20c709ea59f27c0ae25e42308b7beb2153f8b
aashana94 <aashananijhawan@gmail.com>
2018-10-08 23:04:44 +0200

    import library

--- R/geoCode.R
+++ R/geoCode.R
@@ -1,34 +1,39 @@
-library(httr)
-library(jsonlite)
-
+#' Making a linear regression function using S3 class
+#' @importFrom httr jsonlite GET fromJSON content
+#' @param address as vector
+#' @param apikey as character
+#'
+#' @return vector
+#' @export
+#' @examples latlon(address = "New Delhi, India", apiKey = "randomenumbersandletters")
 latlon <- function(address, apiKey){
 
   param <- gsub("[\\@\\#\\%\\&\\!\\(\\)\\_\\*\\+\\;\\^]", "", address)
   params <- trimws(gsub("\\s+","+", param))
 
   base <- "https://maps.googleapis.com/maps/api/geocode/json?address="
-  
+
   urlAPI <- paste0(base,params,"&key=",apiKey)
-  callAPI <- GET(urlAPI)
-  content(callAPI)
-  resultJSON <- fromJSON(txt = callAPI$url)
+  callAPI <- httr::GET(urlAPI)
+  httr::content(callAPI)
+  resultJSON <- jsonlite::fromJSON(txt = callAPI$url)
   X <- resultJSON$results$geometry$location$lng
   Y <- resultJSON$results$geometry$location$lat
 
   return(c(X,Y))
 }
-
+latlon(address = "New Delhi, India", apiKey = "randomenumbersandletters")
 
 reverselatlon<- function(lat, lon, apiKey){
 
   params <- paste0(lat,",",lon)
   base <- "https://maps.googleapis.com/maps/api/geocode/json?latlng="
-  
+
   urlAPI <- paste0(base,params,"&key=",apiKey)
-  callAPI <- GET(urlAPI)
-  content(callAPI)
-  resultJSON <- fromJSON(txt = callAPI$url)
+  callAPI <- httr::GET(urlAPI)
+  httr::content(callAPI)
+  resultJSON <- jsonlite::fromJSON(txt = callAPI$url)
   address <- resultJSON$results$formatted_address
-  
+
   return(address)
 }
